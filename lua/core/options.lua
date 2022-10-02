171e41e6fda9721d642f9f3ce53fb95fc3625860
Adones Pitogo <adonesp@Adoness-MacBook-Pro.local>
2022-10-03 02:11:46 +0800

    disable clipboard

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -18,7 +18,7 @@ opt.encoding = 'UTF-8'
 -- General
 -----------------------------------------------------------
 opt.mouse = 'a'                       -- Enable mouse support
-opt.clipboard = 'unnamedplus'         -- Copy/paste to system clipboard
+-- opt.clipboard = 'unnamedplus'         -- Copy/paste to system clipboard
 opt.swapfile = false                  -- Don't use swapfile
 opt.completeopt = 'menuone,noinsert,noselect'  -- Autocomplete options
 
