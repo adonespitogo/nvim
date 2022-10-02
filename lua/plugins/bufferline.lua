0ee44d40f997f05d04a4936b091f5c0ef9752ec3
Adones Pitogo <adonesp@Adoness-MacBook-Pro.local>
2022-10-03 06:32:47 +0800

    bufferline offset

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,7 +1,8 @@
 vim.opt.termguicolors = true
 
 require("bufferline").setup({
-	options = {
-		diagnostics = "nvim_lsp",
-	},
+  options = {
+    diagnostics = "nvim_lsp",
+    offsets = { { filetype = "NvimTree", text = "", padding = 1 } }
+  },
 })
