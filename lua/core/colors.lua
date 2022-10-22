d23f362a37e1c5695c035a8d0d052362341366b8
Adones Pitogo <pitogo.adones@gmail.com>
2022-10-22 12:00:52 +0800

    fix tokyonight borders

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -1,5 +1,14 @@
 -----------------------------------------------------------
 -- Color schemes configuration file
 -----------------------------------------------------------
+require("tokyonight").setup({
+  style = "night",
+  styles = {
+    sidebars = "transparent"
+  },
+  on_colors = function(colors)
+    colors.border = colors.dark3
+  end,
+})
 
-vim.cmd [[colorscheme tokyonight-night]]
+vim.cmd([[ colorscheme tokyonight ]])
