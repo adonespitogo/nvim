806c87092bed04bc47f2c16a5db69aac4e30ed92
Adones Pitogo <pitogo.adones@gmail.com>
2022-10-23 14:46:41 +0800

    install argtextobject.vim

--- lua/plug_init.lua
+++ lua/plug_init.lua
@@ -4,29 +4,31 @@ local fn = vim.fn
 local install_path = (vim.env.XDG_DATA_HOME or (vim.env.HOME .. "/.local/share")) .. "/nvim/site/autoload/plug.vim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  print("Installing vim-plug...")
-  PlugBootstrap = fn.system({
-    "curl",
-    "-fLo",
-    install_path,
-    "--create-dirs",
-    "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
-  })
+	print("Installing vim-plug...")
+	PlugBootstrap = fn.system({
+		"curl",
+		"-fLo",
+		install_path,
+		"--create-dirs",
+		"https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
+	})
 end
 
 local Plug = vim.fn["plug#"]
 
 vim.call("plug#begin", "~/.config/nvim/plugged")
+
+------------------------------------------
 -- Install plugins not supported by packer
 ------------------------------------------
 Plug("tpope/vim-surround")
 Plug("preservim/nerdcommenter")
 Plug("moll/vim-bbye")
+Plug("vim-scripts/argtextobj.vim") -- provides 'a' (argument) text object
 ------------------------------------------
+
 vim.call("plug#end")
 
 if PlugBootstrap then
-  vim.cmd([[
-      PlugInstall
-    ]])
+	vim.cmd([[ PlugInstall ]])
 end
