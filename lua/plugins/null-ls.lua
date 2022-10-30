76dfa2f62db92fcfeeee4a2d9dc5561b1c803fce
Adones Pitogo <pitogo.adones@gmail.com>
2022-10-31 07:22:39 +0800

    eslint conditions

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -8,6 +8,8 @@ local diagnostics = null_ls.builtins.diagnostics
 local actions = null_ls.builtins.code_actions
 local completion = null_ls.builtins.completion
 
+local eslint_files = { ".eslintrc", ".eslintrc.json", ".eslintrc.js", ".eslintrc.yml" }
+
 local sources = {
   -- actions
   actions.eslint,
@@ -18,37 +20,45 @@ local sources = {
 
   -- linters
   diagnostics.luacheck.with({
-    extra_args = '--globals vim'
+    extra_args = "--globals vim",
   }),
-  diagnostics.eslint,
+  diagnostics.eslint.with({
+    condition = function(utils)
+      return utils.root_has_file(eslint_files)
+    end,
+  }),
   diagnostics.erb_lint,
   diagnostics.jsonlint,
   diagnostics.yamllint.with({
-    filetypes = { 'yaml', 'yml' }
+    filetypes = { "yaml", "yml" },
   }),
   diagnostics.proselint.with({
-    filetypes = { 'text' }
+    filetypes = { "text" },
   }),
   diagnostics.haml_lint,
 
   -- formatters
   formatting.prettierd.with({
-    filetypes = { 'html', 'css', 'scss' }
+    filetypes = { "html", "css", "scss" },
   }),
-  formatting.eslint,
+  formatting.eslint.with({
+    condition = function(utils)
+      return utils.root_has_file(eslint_files)
+    end,
+  }),
   formatting.erb_lint,
   formatting.autopep8,
   formatting.stylelint,
   formatting.stylua,
   formatting.beautysh,
-  formatting.codespell
+  formatting.codespell,
 }
 
 local opts = { noremap = true, silent = true }
 
 ---@diagnostic disable-next-line: unused-local
 local on_attach = function(client, bufnr)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ';a', '<CMD>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", ";a", "<CMD>lua vim.lsp.buf.code_action()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<F3>", ":Format<CR>", opts)
   vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format { async = true }' ]])
 end
