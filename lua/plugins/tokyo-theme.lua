6cac4fa2504ffd481993ffabc61455ed4275b673
Adones Pitogo <pitogo.adones@gmail.com>
2023-08-19 05:42:36 +0800

    tokyo night theme - black bg

--- lua/plugins/tokyo-theme.lua
+++ lua/plugins/tokyo-theme.lua
@@ -9,6 +9,15 @@ require("tokyonight").setup({
 
   -- Add border between file tree and document
   on_colors = function(colors)
+    colors.bg = "#040603"
+    colors.bg_dark = "#030305"
+    colors.bg_float = colors.bg_dark
+    colors.bg_highlight = "#292e42"
+    colors.bg_popup = colors.bg_dark
+    colors.bg_search = "#3d59a1"
+    colors.bg_sidebar = colors.bg_dark
+    colors.bg_statusline = colors.bg_dark
+    colors.bg_visual = "#283457"
     colors.border = colors.blue
   end,
 })
