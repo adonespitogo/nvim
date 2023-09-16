a5588ac1f41da9653b6bfd2ce217fe93b5137116
Adones Pitogo <pitogo.adones@gmail.com>
2023-09-17 05:42:14 +0800

    leader+cc to toggle comments

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -5,6 +5,20 @@ return {
 	},
 	lazy = false,
 	config = function()
-		require("Comment").setup()
+		require("Comment").setup({
+			toggler = {
+				---Line-comment toggle keymap
+				line = "<leader>cc",
+				---Block-comment toggle keymap
+				block = "<leader>cb",
+			},
+			---LHS of operator-pending mappings in NORMAL and VISUAL mode
+			opleader = {
+				---Line-comment toggle keymap
+				line = "<leader>cc",
+				---Block-comment toggle keymap
+				block = "<leader>cb",
+			},
+		})
 	end,
 }
