4c23a363dc14a603b560341941b05beb1c29a6b6
Adones Pitogo <pitogo.adones@gmail.com>
2023-09-22 13:59:08 +0800

    install colorizer, integrate comment.nvim with treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,38 +1,48 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })
-	end,
-	config = function()
-		local nvim_treesitter = require("nvim-treesitter.configs")
-		nvim_treesitter.setup({
-			-- A list of parser names, or "all"
-			ensure_installed = {
-				"bash",
-				"c",
-				"cpp",
-				"css",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"python",
-				"rust",
-				"typescript",
-				"vim",
-				"yaml",
-				"go",
-				"php",
-			},
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
-			highlight = {
-				-- `false` will disable the whole extension
-				enable = true,
-			},
-		})
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })
+    end,
+    config = function()
+        local nvim_treesitter = require("nvim-treesitter.configs")
+        nvim_treesitter.setup({
+            -- Disable autocmd for commentstring
+            -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#plugins-with-a-pre-comment-hook
+            context_commentstring = {
+                enable = true,
+                enable_autocmd = false,
+            },
 
-		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-		parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
-	end,
+            -- A list of parser names, or "all"
+            ensure_installed = {
+                "bash",
+                "c",
+                "cpp",
+                "css",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "python",
+                "rust",
+                "typescript",
+                "vim",
+                "yaml",
+                "go",
+                "php",
+            },
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
+            highlight = {
+                -- `false` will disable the whole extension
+                enable = true,
+            },
+        })
+
+        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+    end,
 }
