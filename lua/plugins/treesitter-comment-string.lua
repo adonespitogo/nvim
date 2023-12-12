0eae3fff540aad4082f1aac314b8c3ce997c12f4
Adones Pitogo <pitogo.adones@gmail.com>
2023-12-12 16:05:28 +0800

    updated nvim plugins

--- lua/plugins/treesitter-comment-string.lua
+++ lua/plugins/treesitter-comment-string.lua
@@ -1,14 +1,10 @@
 return {
-	"JoosepAlviste/nvim-ts-context-commentstring",
-	lazy = true,
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- Disable autocmd for commentstring
-			-- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#plugins-with-a-pre-comment-hook
-			context_commentstring = {
-				enable = true,
-				enable_autocmd = false,
-			},
-		})
-	end,
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+    config = function()
+        require("ts_context_commentstring").setup({
+            enable = true,
+            enable_autocmd = false,
+        })
+    end,
 }
