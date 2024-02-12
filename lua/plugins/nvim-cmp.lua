a6715fcf4a8de175437573799fb9a065ec33ff75
Adones Pitogo <pitogo.adones@gmail.com>
2024-02-13 04:33:09 +0800

    modify tab behaviour

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -61,11 +61,11 @@ return {
 
 			-- Key mapping
 			mapping = {
-				-- ["<C-n>"] = cmp.mapping.select_next_item(),
-				-- ["<C-p>"] = cmp.mapping.select_prev_item(),
-				-- ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-				-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-				-- ["<C-o>"] = cmp.mapping.complete(),
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-l>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.close(),
 				["<CR>"] = cmp.mapping.confirm({
 					behavior = cmp.ConfirmBehavior.Replace,
@@ -74,23 +74,23 @@ return {
 
 				-- Tab mapping
 				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() and has_words_before() then
+					if cmp.visible() then
 						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
 					elseif luasnip.expand_or_jumpable() then
 						luasnip.expand_or_jump()
-					elseif cmp.visible() then
-						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+					elseif has_words_before() then
+						cmp.complete()
 					else
 						fallback()
 					end
 				end),
 
 				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() and has_words_before() then
+					if cmp.visible() then
 						cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
 					elseif luasnip.jumpable(-1) then
 						luasnip.jump(-1)
-					elseif cmp.visible() then
+					elseif has_words_before() then
 						cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
 					else
 						fallback()
