317fae16cd4c1709c374e0e5129d56b93180fdf8
Adones Pitogo <pitogo.adones@gmail.com>
2024-04-26 23:37:58 +0800

    fix nvim-html-css plugin

---
+++ lua/plugins/nvim-html-css.lua
@@ -0,0 +1,11 @@
+return {
+    "Jezda1337/nvim-html-css",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-lua/plenary.nvim",
+        "sharkdp/fd",
+    },
+    config = function()
+        require("html-css"):setup()
+    end,
+}
