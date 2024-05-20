7e9a8fa0ae9ae9133670f0595051b73392b54d66
Adones Pitogo <pitogo.adones@gmail.com>
2024-05-21 07:37:59 +0800

    fix diagnostics indicator

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -10,21 +10,12 @@ return {
 	},
 	config = function()
 		require("barbecue").setup({
-			attach_navic = false, -- prevent barbecue from automatically attaching nvim-navic
 			show_dirname = false,
 			show_basename = true,
 			show_modified = true,
 			show_diagnostics = true,
 			lead_custom_section = function(_, winr)
 				return " " .. vim.api.nvim_win_get_number(winr) .. " "
-			end,
-		})
-
-		require("lspconfig")["nvim-navic"].setup({
-			on_attach = function(client, bufnr)
-				if client.server_capabilities["documentSymbolProvider"] then
-					require("nvim-navic").attach(client, bufnr)
-				end
 			end,
 		})
 	end,
