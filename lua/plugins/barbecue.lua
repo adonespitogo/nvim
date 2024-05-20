660d7ff83c3bc34f2cbb5e4fa1e3842a4ef7abc0
Adones Pitogo <pitogo.adones@gmail.com>
2024-05-21 07:32:59 +0800

    updated barbecue, use red filename

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -1,32 +1,31 @@
 -- winbar plugin
 return {
-    "adonespitogo/barbecue.nvim",
-    -- dir = "~/Projects/barbecue.nvim",
-    name = "barbecue",
-    branch = "feature/show-diagnostics",
-    version = "*",
-    dependencies = {
-        "SmiteshP/nvim-navic",
-        "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    config = function()
-        require("barbecue").setup({
-            -- attach_navic = false, -- prevent barbecue from automatically attaching nvim-navic
-            show_dirname = false,
-            show_basename = true,
-            show_modified = true,
-            show_diagnostics = true,
-            lead_custom_section = function(_, winr)
-                return " " .. vim.api.nvim_win_get_number(winr) .. " "
-            end,
-        })
+	"adonespitogo/barbecue.nvim",
+	name = "barbecue",
+	branch = "feature/show-diagnostics",
+	version = "*",
+	dependencies = {
+		"SmiteshP/nvim-navic",
+		"nvim-tree/nvim-web-devicons", -- optional dependency
+	},
+	config = function()
+		require("barbecue").setup({
+			attach_navic = false, -- prevent barbecue from automatically attaching nvim-navic
+			show_dirname = false,
+			show_basename = true,
+			show_modified = true,
+			show_diagnostics = true,
+			lead_custom_section = function(_, winr)
+				return " " .. vim.api.nvim_win_get_number(winr) .. " "
+			end,
+		})
 
-        -- require("lspconfig")["nvim-navic"].setup({
-        -- 	on_attach = function(client, bufnr)
-        -- 		if client.server_capabilities["documentSymbolProvider"] then
-        -- 			require("nvim-navic").attach(client, bufnr)
-        -- 		end
-        -- 	end,
-        -- })
-    end,
+		require("lspconfig")["nvim-navic"].setup({
+			on_attach = function(client, bufnr)
+				if client.server_capabilities["documentSymbolProvider"] then
+					require("nvim-navic").attach(client, bufnr)
+				end
+			end,
+		})
+	end,
 }
