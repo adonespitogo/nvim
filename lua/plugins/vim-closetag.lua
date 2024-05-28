c9b7249fb9b8427a8220bf44656141f258e138eb
Adones Pitogo <pitogo.adones@gmail.com>
2024-05-28 17:00:23 +0800

    added templ to closetags

--- lua/plugins/vim-closetag.lua
+++ lua/plugins/vim-closetag.lua
@@ -3,6 +3,7 @@ return {
 	event = "BufRead",
 	setup = function()
 		vim.g.closetag_emptyTags_caseSensitive = 1
+		vim.g.closetag_filenames = "*.html,*.xhtml,*.phtml,*.templ"
 		-- Some other settings
 	end,
 }
