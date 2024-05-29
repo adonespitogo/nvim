e90acfecb84e80ba5e9eda5c3151e39b0a274af1
Adones Pitogo <pitogo.adones@gmail.com>
2024-05-29 16:10:10 +0800

    can auto-rename html tags with nvim-ts-autotag

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -1,22 +1,22 @@
 -- winbar plugin
 return {
-	"adonespitogo/barbecue.nvim",
-	name = "barbecue",
-	branch = "main",
-	version = "*",
-	dependencies = {
-		"SmiteshP/nvim-navic",
-		"nvim-tree/nvim-web-devicons", -- optional dependency
-	},
-	config = function()
-		require("barbecue").setup({
-			show_dirname = false,
-			show_basename = true,
-			show_modified = true,
-			show_diagnostics = true,
-			lead_custom_section = function(_, winr)
-				return " " .. vim.api.nvim_win_get_number(winr) .. " "
-			end,
-		})
-	end,
+    "adonespitogo/barbecue.nvim",
+    name = "barbecue",
+    branch = "main",
+    version = "*",
+    dependencies = {
+        "SmiteshP/nvim-navic",
+        "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    config = function()
+        require("barbecue").setup({
+            show_dirname = false,
+            show_basename = true,
+            show_modified = true,
+            show_diagnostics = true,
+            lead_custom_section = function(_, winr)
+                return " " .. vim.api.nvim_win_get_number(winr) .. " "
+            end,
+        })
+    end,
 }
