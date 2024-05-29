fefd37b47fd28a37029e9323a6f8ac8c9a93eb0d
Adones Pitogo <pitogo.adones@gmail.com>
2024-05-29 23:30:36 +0800

    fix: barbecue diagnostics red color

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -1,22 +1,25 @@
 -- winbar plugin
 return {
-    "adonespitogo/barbecue.nvim",
-    name = "barbecue",
-    branch = "main",
-    version = "*",
-    dependencies = {
-        "SmiteshP/nvim-navic",
-        "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    config = function()
-        require("barbecue").setup({
-            show_dirname = false,
-            show_basename = true,
-            show_modified = true,
-            show_diagnostics = true,
-            lead_custom_section = function(_, winr)
-                return " " .. vim.api.nvim_win_get_number(winr) .. " "
-            end,
-        })
-    end,
+	"adonespitogo/barbecue.nvim",
+	name = "barbecue",
+	branch = "main",
+	version = "*",
+	dependencies = {
+		"SmiteshP/nvim-navic",
+		"nvim-tree/nvim-web-devicons", -- optional dependency
+	},
+	config = function()
+		require("barbecue").setup({
+			theme = {
+				diagnostics = { fg = "#ff0000" },
+			},
+			show_dirname = false,
+			show_basename = true,
+			show_modified = true,
+			show_diagnostics = true,
+			lead_custom_section = function(_, winr)
+				return " " .. vim.api.nvim_win_get_number(winr) .. " "
+			end,
+		})
+	end,
 }
