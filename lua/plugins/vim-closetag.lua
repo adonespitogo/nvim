f497a3e6982a4fda65fff0598a17b94181263380
Adones Pitogo <pitogo.adones@gmail.com>
2024-05-29 16:12:37 +0800

    remove vim-closetag in favor of nvim-ts-autotag

--- lua/plugins/vim-closetag.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"alvan/vim-closetag",
-	event = "BufRead",
-	setup = function()
-		vim.g.closetag_emptyTags_caseSensitive = 1
-		vim.g.closetag_filenames = "*.html,*.xhtml,*.phtml,*.templ"
-		-- Some other settings
-	end,
-}
