8e51ca6d7fe8381402c702317c87634e79095f0e
Adones Pitogo <pitogo.adones@gmail.com>
2024-06-20 22:10:45 +0800

    fix: bufferline transparent bg

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,6 +7,27 @@ return {
 		"kyazdani42/nvim-web-devicons",
 	},
 	config = function()
+		-- Set the highlight groups for bufferline to have a transparent background
+		vim.cmd([[
+          highlight BufferLineFill guibg=none
+          highlight BufferLineBackground guibg=none
+          highlight BufferLineBufferVisible guibg=none
+          highlight BufferLineBufferSelected guibg=none
+          highlight BufferLineTab guibg=none
+          highlight BufferLineTabSelected guibg=none
+          highlight BufferLineTabClose guibg=none
+          highlight BufferLineDuplicate guibg=none
+          highlight BufferLineDuplicateSelected guibg=none
+          highlight BufferLineDuplicateVisible guibg=none
+          highlight BufferLineModified guibg=none
+          highlight BufferLineModifiedSelected guibg=none
+          highlight BufferLineModifiedVisible guibg=none
+          highlight BufferLineSeparator guibg=none
+          highlight BufferLineSeparatorSelected guibg=none
+          highlight BufferLineSeparatorVisible guibg=none
+          highlight BufferLineIndicatorSelected guibg=none
+        ]])
+
 		require("bufferline").setup({
 			options = {
 				highlights = {
@@ -38,26 +59,5 @@ return {
 			map("n", "<leader>bv" .. i, ":vsplit<CR>:BufferLineGoToBuffer" .. i .. "<CR>")
 			map("n", "<leader>bh" .. i, ":split<CR>:BufferLineGoToBuffer" .. i .. "<CR>")
 		end
-
-		-- Set the highlight groups for bufferline to have a transparent background
-		vim.cmd([[
-          highlight BufferLineFill guibg=none
-          highlight BufferLineBackground guibg=none
-          highlight BufferLineBufferVisible guibg=none
-          highlight BufferLineBufferSelected guibg=none
-          highlight BufferLineTab guibg=none
-          highlight BufferLineTabSelected guibg=none
-          highlight BufferLineTabClose guibg=none
-          highlight BufferLineDuplicate guibg=none
-          highlight BufferLineDuplicateSelected guibg=none
-          highlight BufferLineDuplicateVisible guibg=none
-          highlight BufferLineModified guibg=none
-          highlight BufferLineModifiedSelected guibg=none
-          highlight BufferLineModifiedVisible guibg=none
-          highlight BufferLineSeparator guibg=none
-          highlight BufferLineSeparatorSelected guibg=none
-          highlight BufferLineSeparatorVisible guibg=none
-          highlight BufferLineIndicatorSelected guibg=none
-        ]])
 	end,
 }
