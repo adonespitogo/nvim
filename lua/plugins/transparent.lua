81c685b36cbb4bb80ccd36d431b68b8a53e504a7
Adones Pitogo <pitogo.adones@gmail.com>
2024-10-22 22:38:15 +0800

    fix: nvim-html-css, which-key for nvim 0.11.x

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,10 +1,18 @@
 return {
 	"xiyaowong/transparent.nvim",
 	config = function()
+		-- https://github.com/xiyaowong/transparent.nvim?tab=readme-ov-file#gtransparent_groups
+		vim.g.transparent_groups = vim.list_extend(
+			vim.g.transparent_groups or {},
+			vim.tbl_map(function(v)
+				return v.hl_group
+			end, vim.tbl_values(require("bufferline.config").highlights))
+		)
+
 		local transparent = require("transparent")
 		transparent.setup({})
 		transparent.clear_prefix("BufferLine")
-		-- transparent.clear_prefix("NvimTree")
-		-- transparent.clear_prefix('lualine')
+		transparent.clear_prefix("NvimTree")
+		-- transparent.clear_prefix("lualine")
 	end,
 }
