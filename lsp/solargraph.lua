6715df84e19810903ffa8916efa3b7c5f99207da
Adones Pitogo <pitogo.adones@gmail.com>
2025-10-09 06:12:29 +0800

    fix solargraph root_dir

--- lsp/solargraph.lua
+++ lsp/solargraph.lua
@@ -3,8 +3,8 @@ local on_attach = require("utils.lsp.on_attach")
 local root_dir = require("utils.root-dir")
 
 return {
+  cmd = {"solargraph", "stdio" },
 	on_attach = on_attach,
-	root_dir = root_dir,
 	capabilities = capabilities,
 	filetypes = {
 		"ruby",
