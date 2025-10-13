226126eb4d25ab9c240eefcc861d833feaece22c
Adones Pitogo <pitogo.adones@gmail.com>
2025-10-14 03:14:42 +0800

    fix templ

--- lsp/templ.lua
+++ lsp/templ.lua
@@ -1,16 +1,11 @@
 local capabilities = require("utils.lsp.capabilities")
 local on_attach = require("utils.lsp.on_attach")
-local root_dir = require("utils.root-dir")
 
-local golang_root = require("utils.golang_root")
 local load_goplsrc = require("utils.load_goplsrc")
 local gopls = load_goplsrc()
 
 return {
-	cmd = { "/home/adonesp/.local/bin/templ", "lsp" },
-	filetypes = { "templ" },
 	on_attach = on_attach,
-	root_dir = golang_root,
 	capabilities = capabilities,
 	settings = {
 		gopls = gopls,
