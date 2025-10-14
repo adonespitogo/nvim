59c0ae2084a51f11fe9da7b6f66fd464b47ca554
Adones Pitogo <pitogo.adones@gmail.com>
2025-10-15 02:45:28 +0800

    removed refactoring.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -18,40 +18,6 @@ return {
 		local actions = require("telescope.actions")
 		local builtin = require("telescope.builtin")
 
-		--telescope.setup {
-		--  defaults = {
-		--    mappings = {
-		--      n = {
-		--        ["q"] = actions.close
-		--      },
-		--    },
-		--  },
-		--}
-		--
-		--keymaps
-		--vim.keymap.set('n', ';f',
-		--  function()
-		--    builtin.find_files({
-		--      no_ignore = false,
-		--      hidden = true
-		--    })
-		--  end)
-		--vim.keymap.set('n', ';r', function()
-		--  builtin.live_grep()
-		--end)
-		--vim.keymap.set('n', '\\\\', function()
-		--  builtin.buffers()
-		--end)
-		--vim.keymap.set('n', ';t', function()
-		--  builtin.help_tags()
-		--end)
-		--vim.keymap.set('n', ';;', function()
-		--  builtin.resume()
-		--end)
-		--vim.keymap.set('n', ';e', function()
-		--  builtin.diagnostics()
-		--end)
-
 		telescope.setup({
 			defaults = {
 				sorting_strategy = "ascending",
