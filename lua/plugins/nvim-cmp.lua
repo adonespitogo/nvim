478a4f1c28088ffcc22f709b02ea9a1d28cccc9c
Adones Pitogo <pitogo.adones@gmail.com>
2025-10-15 18:09:56 +0800

    enable bootstrap 5 lsp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -90,12 +90,12 @@ return {
 				{
 					name = "html-css",
 					option = {
-						enable_on = { "html", "vue", "ejs", "pug", "eruby", "haml" },
+						enable_on = { "html", "vue", "ejs", "pug", "eruby", "haml", "templ" },
 						enable_file_patterns = { "*.html", "*.erb" },
 						file_extensions = { "css", "sass", "less" }, -- set the local filetypes from which you want to derive classes
 						style_sheets = {
 							-- example of remote styles, only css no js for now
-							-- "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+							"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
 
 							-- TODO: try load file when offline
 							-- "file://"
