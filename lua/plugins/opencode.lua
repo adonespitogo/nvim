122f5c8710877a814b313483a6ea0a187fbcf735
Adones Pitogo <pitogo.adones@gmail.com>
2025-11-01 12:06:07 +0800

    Revert "clean up"
    This reverts commit 88bd39e6de6d81d0880a28e89d88ecc1375afa96.

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -3,13 +3,16 @@ return {
 	dependencies = {
 		-- Recommended for `ask()` and `select()`.
 		-- Required for default `toggle()` implementation.
-		{ "folke/snacks.nvim", opts = { auto_reload = true } },
+		{ "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
 	},
 	config = function()
 		---@type opencode.Opts
 		vim.g.opencode_opts = {
 			-- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
 		}
+
+		-- Required for `opts.auto_reload`.
+		vim.o.autoread = true
 
 		vim.keymap.set({ "n", "x" }, "<leader>ck", function()
 			require("opencode").ask("@this: ", { submit = true })
@@ -26,5 +29,6 @@ return {
 		vim.keymap.set("n", "<leader>cd", function()
 			require("opencode").prompt("@diagnostics: ", { submit = true })
 		end, { desc = "Prompt diagnostics opencode" })
+
 	end,
 }
